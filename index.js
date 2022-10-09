e7754b49b56e7333ec2084a3ae350226e75718a3
Swagam Dasgupta <swagam.dasgupta@gmail.com>
2022-10-09 18:41:57 +0530

    render function complete

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
+    renderLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.textContent += myLeads[i] + " "
+
+function renderLeads(){
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+        // Another way to add html to parent element
+        //create + add text + append to parent (given below)
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+    }
+
+    ulEl.innerHTML = listItems
 }
