dfd20a27467c8e40f7a75a04422a6fa617bdb92e
Swagam Dasgupta <swagam.dasgupta@gmail.com>
2022-10-09 16:58:30 +0530

    basic funtionality done

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
-
-inputBtn = document.getElementById("input-btn")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
-    console.log("Button Clicked")
+    myLeads.push(inputEl.value)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.textContent += myLeads[i] + " "
+}
